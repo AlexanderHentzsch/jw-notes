3ed72f45e93207d940c7e80a7dacdaf19c62e33d
Alexander Hentzsch <a.hentzsch@hotmail.de>
2025-02-15 11:37:52 +0100

    Vuetify integriert
    - textarea durch v-textarea ersetzt
    - Namensanpassung in der Lizenz
    - Ausgabe der Versionsnummer
    - GitHub Verlinkung in Textfluss integriert

---
+++ components/NodeTextInput.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {store} from '~/stores/default.js';
+
+function nothing() {
+}
+
+const textarea = useTemplateRef('textarea');
+
+function focus() {
+  textarea.value.focus();
+}
+
+defineExpose({focus});
+</script>
+
+<template>
+  <v-textarea
+    v-model="store.currentNode"
+    @keydown.enter.prevent="nothing"
+    ref="textarea"
+    autofocus
+    auto-grow
+    clearable
+    persistent-clear
+    density="compact"
+    flat
+    hide-details="auto"
+    :rows="1"
+    :max-rows="4"
+    no-resize
+    variant="outlined"
+  ></v-textarea>
+</template>
